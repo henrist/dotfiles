0a38a671789ac6231390a820eea04c766195b513
Henrik Steen <henrist@henrist.net>
2016-07-11 17:34:30 +0200

    Update vim-setup to allow for upgrading

--- vim/install.sh
+++ vim/install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 . $(dirname $0)/../include.sh
 
-if [ -e ~/.bashrc ]; then
-    echo "Vim is already set up"
-    exit 2
-fi
-
-echo "Setting up vim-config"
-
 VIMRC="$(pwd)/vimrc"
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ -e ~/.vimrc ]; then
+    echo "Vim is already set up, trying to upgrade"
+
+    cd ~/.vim/bundle/Vundle.vim
+    git pull
+else
+    echo "Setting up vim-config"
+
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 vim -u $VIMRC +PluginInstall +qall
 
-ln -s $VIMRC ~/.vimrc
+if ! [ -e ~/.vimrc ]; then
+    ln -s $VIMRC ~/.vimrc
+fi
