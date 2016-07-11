074eb1179640626575375c12b368b287ae9e1f4e
Henrik Steen <henrist@henrist.net>
2016-07-11 18:05:14 +0200

    Update bashrc with git-support and more

--- bash/install.sh
+++ bash/install.sh
@@ -2,9 +2,15 @@
 . $(dirname $0)/../include.sh
 
 if [ -e ~/.bashrc ] && grep -q "myconfig/bash" ~/.bashrc; then
-    info "Bash is already set up"
-    exit 2
+    info "bashrc is already set up"
+else
+    echo ". ~/myconfig/bash/bashrc" >>~/.bashrc
+    info "bashrc has been set up"
 fi
 
-echo ". ~/myconfig/bash/bashrc" >>~/.bashrc
-info "Bash has been set up"
+# (the following should be available through git bash competion)
+#GIT_PROMPT=~/myconfig/bash/git-prompt.sh
+#if ! [ -e $GIT_PROMPT ]; then
+#    wget -qO $GIT_PROMPT https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+#    info "fetched git-prompt.sh from github"
+#fi
