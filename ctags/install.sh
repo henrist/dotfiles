2e8f668c283da508d8becfbc525ac3f412883585
Henrik Steen <henrist@henrist.net>
2016-08-06 18:53:55 +0200

    add ctags

---
+++ ctags/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. $(dirname $0)/../include.sh
+
+if [ -e ~/.ctags ]; then
+    info "ctags is already set up"
+else
+    ln -s ~/myconfig/ctags/ctags ~/.ctags
+    sudo apt-get install ctags
+    info "ctags has been set up"
+fi
