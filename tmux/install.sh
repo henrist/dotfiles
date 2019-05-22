6a408a03b04ef69f64f67c045073370d82b8524e
Henrik Steen <henrist@henrist.net>
2019-05-22 19:13:53 +0200

    tmux 2.9 compat
    Split out to a separate file for simplicity.

--- tmux/install.sh
+++ tmux/install.sh
@@ -7,4 +7,9 @@ fi
 
 echo "Setting up tmux-config"
 
-ln -s "$(pwd)/tmux.conf" ~/.tmux.conf
+file=tmux.conf
+if test $(echo "$(tmux -V | cut -d " " -f2 | tr -d -c '[0-9.]') >= 2.9" | bc) -eq 1; then
+  file=tmux-2.9.conf
+fi
+
+ln -s "$(pwd)/$file" ~/.tmux.conf
