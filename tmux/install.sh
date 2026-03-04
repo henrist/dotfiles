6815768f88192b2489d5ce4b1d3845c684167acc
Henrik Steen <henrist@henrist.net>
2026-03-04 20:37:08 +0100

    Modernize tmux config
    Drop version-split config and clean legacy tmux options. Keep one portable config with cross-platform clipboard copy and simplify installer logic.

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 if [ -e ~/.tmux.conf ]; then
-    echo "Tmux is already set up"
-    exit
+    printf "~/.tmux.conf already exists\n"
+    exit 0
 fi
 
-echo "Setting up tmux-config"
-
-file=tmux.conf
-if test $(echo "$(tmux -V | cut -d " " -f2 | tr -d -c '[0-9.]') >= 2.9" | bc) -eq 1; then
-  file=tmux-2.9.conf
-fi
-
-ln -s "$(pwd)/$file" ~/.tmux.conf
+printf "Linking tmux config\n"
+ln -s "$(pwd)/tmux.conf" ~/.tmux.conf
